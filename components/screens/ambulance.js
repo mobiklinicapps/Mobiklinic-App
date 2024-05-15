fede0b23c06341b068cff7f69ce065e8c21b8181
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-05-15 20:10:44 +0300

    add ambulances

--- components/screens/ambulance.js
+++ components/screens/ambulance.js
@@ -1,6 +1,5 @@
-import React, {useEffect, useContext, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Alert,
   View,
   Linking,
   FlatList,
@@ -9,179 +8,95 @@ import {
   StyleSheet,
   StatusBar,
 } from 'react-native';
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import {ListItem} from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Feather';
-import {COLORS} from '../constants/styles';
-
-import {AuthContext} from '../contexts/auth';
-import CustomHeader from '../ui/custom-header';
 import Loader from '../ui/loader';
+import { COLORS } from '../constants/styles';
+import CopyRight from '../simprints/copyright';
 
-const Ambulance = ({navigation}) => {
-  const [state, setState] = useState({
-    isLoading: true,
-    doctors: [],
-    login: false,
-  });
-
-  const {signOut} = useContext(AuthContext);
+const Ambulance = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [ambulances, setAmbulances] = useState([]);
 
   useEffect(() => {
-    _getDoctors();
-
-    return () => {};
+    fetchAmbulances();
   }, []);
 
-  const _getDoctors = async () => {
-    // Fetch doctors from AsyncStorage or API
-
-    // Dummy data for testing
-    const dummyData = [
-      {
-        _id: '1',
-        name: 'Kawoolo Hospital',
-        hospital: 'Driver',
-        status: 1,
-        phone: '+256 772680727',
-      },
-      {
-        _id: '2',
-        name: 'St.Francis Ambulance ',
-        hospital: 'Driver ',
-        status: 1,
-        phone: '+256 755996554',
-      },
-      {
-        _id: '3',
-        name: 'Kinoni Boda',
-        hospital: 'Driver',
-        status: 1,
-        phone: '+256 772954307',
-      },
-      {
-        _id: '4',
-        name: 'Mukonge Health Center Ambulance ',
-        specialisation: 'Clinician',
-        hospital: 'Driver',
-        status: 1,
-        phone: '+256 707367315',
-      },
-      {
-        _id: '5',
-        name: 'St.Francis Ambulance',
-        specialisation: 'General practitioner',
-        hospital: 'Driver ',
-        status: 1,
-        phone: '256 755996554',
-      },
-    ];
-
-    if (dummyData && dummyData.length > 0) {
-      setState({doctors: dummyData, isLoading: false});
-    } else {
-      setState({doctors: [], isLoading: false});
+  const fetchAmbulances = async () => {
+    try {
+      const response = await fetch('https://mk-be-strapi-production.up.railway.app/api/ambulances');
+      const data = await response.json();
+      setAmbulances(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching ambulances:', error);
+      setIsLoading(false);
     }
-
-    // Rest of your code for fetching doctors
-
-    // ...
   };
 
-  const _keyExtractor = item => item._id;
+  const keyExtractor = item => item.id.toString();
 
-  const _renderItem = ({item}) => {
-    return (
-      <TouchableOpacity onPress={() => Linking.openURL(`tel:${item.phone}`)}>
-        <ListItem bottomDivider>
-          <Icon
-            name="circle"
-            size={25}
-            color={
-              item.status === 1
-                ? 'green'
-                : item.status === 2
-                ? COLORS.PRIMARY
-                : '#f00'
-            }
-          />
-          <ListItem.Content>
-            <ListItem.Title style={STYLES.listTitle}>
-              {item.name}
-            </ListItem.Title>
-            <ListItem.Subtitle style={STYLES.subtitle}>
-              <View style={STYLES.wrapper}>
-                <View style={STYLES.subtitle}>
-                  <Text style={STYLES.label}>Phone Number</Text>
-                  <Text style={{color: COLORS.BLACK}}>{item.phone}</Text>
-                </View>
-                <View style={[STYLES.subtitle, {color: COLORS.BLACK}]}>
-                  <Text style={{color: COLORS.BLACK}}>{item.hospital}</Text>
-                </View>
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => Linking.openURL(`tel:${item.attributes.contact}`)}>
+      <ListItem bottomDivider>
+        <Icon
+          name="circle"
+          size={25}
+          color={'green'}
+        />
+        <ListItem.Content>
+          <ListItem.Title style={styles.listTitle}>{item.attributes.name}</ListItem.Title>
+          <ListItem.Subtitle style={styles.subtitle}>
+            <View style={styles.wrapper}>
+              <View style={styles.subtitle}>
+                <Text style={styles.label}>Phone Number</Text>
+                <Text style={{ color: COLORS.BLACK }}>{item.attributes.contact}</Text>
               </View>
-            </ListItem.Subtitle>
-          </ListItem.Content>
-          <Icon name="phone" size={25} color="rgba(0,0,0,.3)" />
-        </ListItem>
-      </TouchableOpacity>
-    );
-  };
-
-  const {doctors, isLoading, login} = state;
-
-  if (login) return <Login />;
+              <View style={[styles.subtitle, { color: COLORS.BLACK }]}>
+                <Text style={{ color: COLORS.BLACK }}>{item.attributes.hospital}</Text>
+              </View>
+            </View>
+          </ListItem.Subtitle>
+        </ListItem.Content>
+        <Icon name="phone" size={25} color="rgba(0,0,0,.3)" />
+      </ListItem>
+    </TouchableOpacity>
+  );
 
   if (isLoading) return <Loader />;
-
-  if (!doctors || doctors.length === 0) {
+  if (!ambulances || ambulances.length === 0) {
     return (
-      <View style={STYLES.container}>
-        <CustomHeader navigation={navigation} title={'Doctors'} />
-        <View>
-          <StatusBar />
-          <Text style={STYLES.textColor}>No Doctors available</Text>
-        </View>
+      <View style={styles.container}>
+        <StatusBar />
+        <Text style={styles.textColor}>No Ambulances available</Text>
       </View>
     );
   }
 
   return (
-    <View style={STYLES.wrapper}>
+    <View style={styles.wrapper}>
       <StatusBar />
-
+      <View>
+        <Text style={styles.header}>AMBULANCES</Text>
+      </View>
       <FlatList
-        data={doctors}
-        renderItem={_renderItem}
-        keyExtractor={_keyExtractor}
+        data={ambulances}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
       />
+      <CopyRight />
     </View>
   );
 };
 
-const STYLES = StyleSheet.create({
+const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: COLORS.SECONDARY,
   },
-  header: {
+  container: {
     flex: 1,
-  },
-  body: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontWeight: 'bold',
-    color: COLORS.SECONDARY,
-    textAlign: 'center',
-  },
-  alert: {
-    color: COLORS.GREY,
-    textAlign: 'center',
-    marginTop: 15,
+    backgroundColor: COLORS.SECONDARY,
   },
   listTitle: {
     fontSize: 16,
@@ -198,20 +113,17 @@ const STYLES = StyleSheet.create({
     marginRight: 5,
     color: COLORS.BLACK,
   },
-  leftHeader: {
-    flex: 1,
-    paddingLeft: 10,
-  },
-  centerHeader: {
-    flex: 2,
-    flexDirection: 'row',
+  textColor: {
     color: COLORS.BLACK,
   },
-  rightHeader: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    color: COLORS.BLACK,
+  header: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: COLORS.PRIMARY,
+    textAlign: 'center',
+    paddingVertical: 20,
+    textDecorationLine: 'underline',
+    backgroundColor: COLORS.WHITE,
   },
 });
 
